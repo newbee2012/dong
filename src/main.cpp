593ebc24c125b790168f0e4afbcebc137507252b
newbee2012 <newbee2012@126.com>
2017-05-25 03:40:22 +0800

    代码格式整理

--- src/main.cpp
+++ src/main.cpp
@@ -7,38 +7,35 @@ using namespace caffe;
 int main()
 {
     db::DB* mydb = db::GetDB("lmdb");
-    mydb->Open("../../data/mnist_train_lmdb",db::READ);
-    db::Cursor* corsor =mydb->NewCursor();
+    mydb->Open("../../data/mnist_train_lmdb", db::READ);
+    db::Cursor* corsor = mydb->NewCursor();
     corsor->SeekToFirst();
     {
         Datum datum;
-        for(int i = 0; i<2 && corsor->valid(); i++)
-        {
+
+        for (int i = 0; i < 2 && corsor->valid(); i++) {
             const string& value = corsor->value();
             datum.ParseFromString(value);
-            int wight =datum.width();
+            int wight = datum.width();
             int height = datum.height();
-            cout<<"image"<<corsor->key()<<",label is "<<datum.label()<<endl;
-            for(int w=0; w<wight; w++)
-            {
-                for(int h=0; h<height; h++)
-                {
-                    char x = (int)(datum.data()[w*height+h])!=0 ? '#':'.';
-                    cout<<x;
-                    cout<<" ";
+            cout << "image" << corsor->key() << ",label is " << datum.label() << endl;
+
+            for (int w = 0; w < wight; w++) {
+                for (int h = 0; h < height; h++) {
+                    char x = (int)(datum.data()[w * height + h]) != 0 ? '#' : '.';
+                    cout << x;
+                    cout << " ";
                 }
 
-                cout<<endl;
+                cout << endl;
             }
 
             corsor->Next();
         }
     }
-
     delete corsor;
     mydb->Close();
     delete mydb;
-
     cout << "Hello world!" << endl;
     return 0;
 }
