1e72077bf77013af1e87d98226536501126cb641
newbee2012 <newbee2012@126.com>
2017-05-25 03:35:30 +0800

    释放资源

--- src/main.cpp
+++ src/main.cpp
@@ -10,29 +10,34 @@ int main()
     mydb->Open("../../data/mnist_train_lmdb",db::READ);
     db::Cursor* corsor =mydb->NewCursor();
     corsor->SeekToFirst();
-    Datum datum;
-    for(int i = 0;i<2 && corsor->valid();i++)
     {
-        const string& value = corsor->value();
-        datum.ParseFromString(value);
-        int wight =datum.width();
-        int height = datum.height();
-        cout<<"image"<<corsor->key()<<",label is "<<datum.label()<<endl;
-        for(int w=0; w<wight; w++)
+        Datum datum;
+        for(int i = 0; i<2 && corsor->valid(); i++)
         {
-            for(int h=0; h<height; h++)
+            const string& value = corsor->value();
+            datum.ParseFromString(value);
+            int wight =datum.width();
+            int height = datum.height();
+            cout<<"image"<<corsor->key()<<",label is "<<datum.label()<<endl;
+            for(int w=0; w<wight; w++)
             {
-                char x = (int)(datum.data()[w*height+h])!=0 ? '#':'.';
-                cout<<x;
-                cout<<" ";
+                for(int h=0; h<height; h++)
+                {
+                    char x = (int)(datum.data()[w*height+h])!=0 ? '#':'.';
+                    cout<<x;
+                    cout<<" ";
+                }
+
+                cout<<endl;
             }
 
-            cout<<endl;
+            corsor->Next();
         }
-
-        corsor->Next();
     }
 
+    delete corsor;
+    mydb->Close();
+    delete mydb;
 
     cout << "Hello world!" << endl;
     return 0;
